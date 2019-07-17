6212185786b4d21fc96e566884b748e8c2336521
AbdelliNasredine <abdou.nasredine@gmail.com>
2019-07-17 03:31:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./public/styles.css">
+    <title>Chat app - AJAX</title>
+</head>
+<body>
+    <div class="container">
+
+    </div>
+</body>
+</html>
